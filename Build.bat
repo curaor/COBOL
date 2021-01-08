5e709b36fd47a3eb7310881556aa09e95a0fc949
curaor <curaor@yahoo.com>
2021-01-08 20:50:29 +0800

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -7,11 +7,11 @@ echo ASCII>> ftpcmd.dat
 
 echo LCD %WORKSPACE% >> ftpcmd.dat
 
-echo PUT DEMO.CBLLE  /QSYS.LIB/OMARCURA11.LIB/QCBLLESRC.FILE/DEMO.MBR >> ftpcmd.dat
+echo PUT DEMO.CBLLE  /QSYS.LIB/%PUB400_LIB%.LIB/QCBLLESRC.FILE/%PUB400_PGM%.MBR >> ftpcmd.dat
 
-echo quote rcmd CHGPFM FILE(OMARCURA11/QCBLLESRC) MBR(DEMO) SRCTYPE(CBLLE) TEXT('DEVOPS WAY TO DEPLOY') >> ftpcmd.dat
+echo quote rcmd CHGPFM FILE(%PUB400_LIB%/QCBLLESRC) MBR(%PUB400_PGM%) SRCTYPE(CBLLE) TEXT('DEVOPS WAY TO DEPLOY') >> ftpcmd.dat
 
-echo quote rcmd CRTBNDCBL PGM(OMARCURA11/DEMO) SRCFILE(OMARCURA11/QCBLLESRC) SRCMBR(DEMO) REPLACE(*YES) OPTION(*EVENTF) DBGVIEW(*SOURCE) >> ftpcmd.dat
+echo quote rcmd CRTBNDCBL PGM(%PUB400_LIB%/%PUB400_PGM%) SRCFILE(%PUB400_LIB%/QCBLLESRC) SRCMBR(%PUB400_PGM%) REPLACE(*YES) OPTION(*EVENTF) DBGVIEW(*SOURCE) >> ftpcmd.dat
 
 echo bye>> ftpcmd.dat
 
@@ -19,4 +19,4 @@ ftp -n -s:ftpcmd.dat PUB400.COM
 
 del "%WORKSPACE%\ftpcmd.dat"
 
-echo "DEMO Program Build Successful!!!"
+echo "%PUB400_PGM% Program Build in library %PUB400_LIB% Successful!!!"
